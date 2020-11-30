74d181b030fa21b4a194cac20405e45fcd947b5b
v1ct1m99@mail.ru <v1ct1m99@mail.ru>
2020-11-30 13:54:36 +0300

    Initial

---
+++ Laboratornaia2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Laboratornaia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пробная программа для тестирования");
+        }
+    }
+}
